7e42c009b47f2b5b2bc4b50b5b53f883f7f7a48b
Albert Vo <albert.vo@gmail.com>
2015-12-28 11:26:35 -0800

    add split4.py

---
+++ split4.py
@@ -0,0 +1,9 @@
+import csv
+import re
+
+fname = "./syslog"
+with open (fname) as fp:
+  for line in fp:
+    (host,d1,d2,date, tz, http1, uri, http2, stat, hitcount) = re.split(' ', line)
+    hitcount = hitcount.strip('\n')
+    print host,hitcount, uri
