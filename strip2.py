425e07b67b5ddc97ebbe55bd1f029117bcff5662
Albert Vo <albert.vo@gmail.com>
2015-12-28 18:58:09 -0800

    added strip2.py

---
+++ strip2.py
@@ -0,0 +1,18 @@
+import re
+
+line = ' this	 is a         line \n'
+
+line = line.strip('\n')
+line = line.strip('\t')
+
+line = line.replace('\n','')
+line = line.replace('\t','')
+
+#line = line.replace('\w+','')
+#line = line.strip('\w+')
+line = line.strip()
+print line
+#line = line.replace(" ","")
+
+line = re.sub(r"\s+", " ", line)
+print line
